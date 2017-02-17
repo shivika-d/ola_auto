98839d5321be4370963660aee80d76de45739263
shivikadhannaia <shivika@minglematic.com>
2017-02-17 16:39:44 +0530

    port changed to 5000

---
+++ config.js
@@ -0,0 +1,14 @@
+
+var Config = {};
+
+
+Config.base_url = "localhost:8081";
+
+Config.port = 5000;
+
+Config.db_username = "ola_auto";
+Config.db_password = "testing";
+Config.db_url = "ds153659.mlab.com:53659/ola_auto";
+
+module.exports = Config;
+
